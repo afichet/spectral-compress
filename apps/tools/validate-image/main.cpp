917a16b69b33d355e93a875d582d54d79488d1e8
Alban Fichet <alban.fichet@gmx.fr>
2022-11-29 12:13:51 +0100

    Consider only non RGB layers and allow exporting catched problematic values to an image mask

--- apps/tools/validate-image/main.cpp
+++ apps/tools/validate-image/main.cpp
@@ -1,23 +1,38 @@
 #include <iostream>
 #include <cmath>
 #include <EXRImage.h>
+#include <lodepng.h>
+#include <cstdint>
+#include <vector>
+#include <string>
 
 int main(int argc, char *argv[])
 {
     if (argc < 2) {
         std::cout << "Usage:" << std::endl
                   << "------" << std::endl
-                  << argv[0] << " <exr_image>" << std::endl;
+                  << argv[0] << " <exr_image> [<mask_err_png>]" << std::endl;
 
         exit(0);
     }
 
     const char* filename_in  = argv[1];
 
+    bool write_mask = false;
+    char* filename_mask = NULL;
+
+    if (argc >= 3) {
+        write_mask = true;
+        filename_mask = argv[2];
+    }
+
     const EXRImage exr_in(filename_in);
 
     const size_t width     = exr_in.width();
     const size_t height    = exr_in.height();
+
+    std::vector<uint8_t> mask_err(width * height * 4);
+    memset(mask_err.data(), 0, width * height * 4);
 
     size_t n_nans     = 0;
     size_t n_inf      = 0;
@@ -26,15 +41,24 @@ int main(int argc, char *argv[])
     const std::vector<EXRFramebuffer*>& framebuffers = exr_in.getFramebuffersConst();
 
     for (const EXRFramebuffer* fb: framebuffers) {
-        for (size_t i = 0; i < width * height; i++) {
-            const float v = fb->getPixelDataConst()[i];
+        // Ignore RGB layers
+        const std::string name = fb->getName();
+        if (name.find("R") && name.find("G") && name.find("B")) {
+            for (size_t i = 0; i < width * height; i++) {
+                const float v = fb->getPixelDataConst()[i];
 
-            if (std::isinf(v)) {
-                ++n_inf;
-            } else if (std::isnan(v)) {
-                ++n_nans;
-            } else if (v < 0) {
-                ++n_neg;
+                if (std::isinf(v)) {
+                    ++n_inf;
+                    mask_err[4 * i + 0] = 255;
+                } else if (std::isnan(v)) {
+                    ++n_nans;
+                    mask_err[4 * i + 1] = 255;
+                } else if (v < 0) {
+                    ++n_neg;
+                    mask_err[4 * i + 2] = 255;
+                }
+
+                mask_err[4 * i + 3] = 255;
             }
         }
     }
@@ -44,6 +68,9 @@ int main(int argc, char *argv[])
     std::cout << "         Inf: " << n_inf << std::endl;
     std::cout << "         Neg: " << n_neg << std::endl;
 
+    if (write_mask) {
+        lodepng::encode(filename_mask, mask_err.data(), width, height);
+    }
 
     return 0;
 }
