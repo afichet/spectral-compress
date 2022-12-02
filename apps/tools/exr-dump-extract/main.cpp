4fbc6c3743dafad4524dc0358fb5a38a01359ab8
Alban Fichet <alban.fichet@gmx.fr>
2022-12-02 15:11:31 +0100

    Missing licence disclaimer

--- apps/tools/exr-dump-extract/main.cpp
+++ apps/tools/exr-dump-extract/main.cpp
@@ -1,3 +1,36 @@
+/**
+ * Copyright 2022 Alban Fichet
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ *   2. Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution.
+ *
+ *   3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <iostream>
 #include <sstream>
 #include <cmath>
@@ -5,7 +38,7 @@
 #include <lodepng.h>
 #include <EXRSpectralImage.h>
 
-int main(int argc, char* argv[]) 
+int main(int argc, char* argv[])
 {
     if (argc < 2) {
         std::cout << "Usage" << std::endl
@@ -30,7 +63,7 @@ int main(int argc, char* argv[])
             for (size_t p = 0; p < img->width() * img->height(); p++) {
                 const float pixel = fb->image_data[p * fb->wavelengths_nm.size() + i];
                 const float value = std::max(0.f, std::min(1.f, std::pow(std::exp2(exposure) * pixel, gamma)));
-                
+
                 out_fb[4 * p + 0] = 255 * value;
                 out_fb[4 * p + 1] = 255 * value;
                 out_fb[4 * p + 2] = 255 * value;
