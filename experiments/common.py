a9089a98ce43c80716c84885dac0c9f202b92ebc
Alban Fichet <alban.fichet@gmx.fr>
2023-01-02 13:57:31 +0100

    Update script to compress the whole Bonn database

--- experiments/common.py
+++ experiments/common.py
@@ -58,14 +58,15 @@ def run_converter_exr_png(input_file, output_file, exposure):
     subprocess.run(args)
 
 
-def run_diff(file_a, file_b, max_err, output_file):
+def run_diff(file_a, file_b, max_err, output_file, diff_error_file):
     fp, fn = os.path.split(output_file)
     os.makedirs(fp, exist_ok=True)
 
     args = [os.path.join(path_bin, 'exr-diff'),
         file_a, file_b,
         output_file,
-        '-u', str(max_err)
+        '-u', str(max_err),
+        '-e', diff_error_file
         ]
 
     subprocess.run(args)
@@ -78,4 +79,29 @@ def get_path_cave(start, dataset_name, technique, n_bits_start, flat_curves):
         technique,
         str(n_bits_start),
         'flat' if flat_curves else 'dynamic'
+    )
+
+
+def get_path_bonn_in(folder, dataset_name, type):
+    return os.path.join(
+        folder,
+        dataset_name,
+        type + '.exr'
+    )
+
+def get_path_bonn_out(
+    start, dataset_name, type,
+    technique,
+    n_bits_start,
+    c_dc, c_ac,
+    flat_q, flat_c):
+    return os.path.join(
+        start,
+        dataset_name,
+        type,
+        technique,
+        str(n_bits_start),
+        str(c_dc), str(c_ac),
+        'q_flat' if flat_q else 'q_dynamic',
+        'c_flat' if flat_c else 'c_dynamic'
     )
--- experiments/compress_bonn.py
+++ experiments/compress_bonn.py
@@ -2,21 +2,26 @@
 
 import os, subprocess
 
+from common import get_path_bonn_in, get_path_bonn_out
+
 path_data = '/home/afichet/spectral_images/EXRs/Bonn/'
 path_bin  = '/home/afichet/Repositories/spectral-compress/build/bin/compress'
 path_out  = 'bonn'
 
 techniques = ['linear', 'unbounded', 'unbounded_to_bounded', 'upperbound', 'twobounds']
-start_bits = [6, 8, 10, 12]
-flat_curves = [True, False]
+start_bits = [8]
+flat_compression = [True, False]
+flat_quantization = [True, False]
 
+c_dc = 0
+c_ac = 1
 
 def run_compressor(
     input_file, output_file,
-    log_file, binlog_file,
+    log_file, binlog_file, dump_file,
     technique,
     n_bits_start, flat_quantization,
-    compression_start, flat_compression):
+    compression_ac, compression_start_dc, flat_compression):
 
     fp, fn = os.path.split(output_file)
     os.makedirs(fp, exist_ok=True)
@@ -28,10 +33,12 @@ def run_compressor(
         spectral_image, output_file,
         '-l', log_file,
         '-k', binlog_file,
+        '-d', dump_file,
         '-m', tech,
         '-q', str(n_bits_start),
-        '-a', str(0.1),
-        '-b', str(compression_start),
+        '-a', str(compression_ac),
+        '-b', str(compression_start_dc),
+        '-e', str(7)
         ]
 
     if flat_quantization:
@@ -44,32 +51,33 @@ def run_compressor(
 
 
 for d in os.listdir(path_data):
-    path_set = os.path.join(path_out, d[:-4])
-
-    for s in ['diffuse', 'specular']:
-        spectral_image = os.path.join(path_data, d, s + '.exr')
-
-        path_img = os.path.join(path_set, s)
+    for type in ['diffuse', 'specular']:
+        spectral_image = get_path_bonn_in(path_data, d, type)
 
         for tech in techniques:
-            path_tech = os.path.join(path_img, tech)
-
             for bits in start_bits:
-                path_bits = os.path.join(path_tech, str(bits))
+                for q_flat in flat_quantization:
+                    for c_flat in flat_compression:
+                        path_final = get_path_bonn_out(
+                            path_out,
+                            d,
+                            type,
+                            tech,
+                            bits,
+                            c_dc, c_ac,
+                            q_flat, c_flat)
 
-                for flat in flat_curves:
-                    if flat:
-                        path_curves = os.path.join(path_bits, 'flat')
-                    else:
-                        path_curves = os.path.join(path_bits, 'dynamic')
+                        output_file = os.path.join(path_final, d[:-4] + '.jxl')
+                        log_file    = os.path.join(path_final, d[:-4] + '.txt')
+                        binlog_file = os.path.join(path_final, d[:-4] + '.bin')
+                        dump_file   = os.path.join(path_final, d[:-4] + '.dat')
 
-                    output_file = os.path.join(path_curves, d[:-4] + '.jxl')
-                    log_file    = os.path.join(path_curves, d[:-4] + '.txt')
-                    binlog_file = os.path.join(path_curves, d[:-4] + '.bin')
+                        print(spectral_image, output_file)
 
-                    print(spectral_image, output_file)
-
-                    run_compressor(
-                        spectral_image, output_file,
-                        log_file, binlog_file,
-                        tech, bits, flat, 0.1, flat)
+                        run_compressor(
+                            spectral_image, output_file,
+                            log_file, binlog_file, dump_file,
+                            tech,
+                            bits, c_flat,
+                            c_dc, c_ac,
+                            False)
