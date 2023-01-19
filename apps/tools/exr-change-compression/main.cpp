53fa4aec48f35fdf20b80c3c4e40b456754f93e0
Alban Fichet <alban.fichet@gmx.fr>
2023-01-19 17:24:35 +0100

    Add a tool to change compression method in OpenEXR image

---
+++ apps/tools/exr-change-compression/main.cpp
@@ -0,0 +1,118 @@
+/**
+ * Copyright 2022 - 2023 Alban Fichet
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ *   2. Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution.
+ *
+ *   3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <tclap/CmdLine.h>
+
+#include <EXRImage.h>
+
+int main(int argc, char* argv[]) {
+
+    std::string path_image_in, path_image_out;
+    Imf::Compression compression;
+
+    try {
+        TCLAP::CmdLine cmd("Change the compression used in an OpenEXR image");
+
+        TCLAP::UnlabeledValueArg<std::string> inputFileArg ("Input", "Path to OpenEXR image read from.", true, "in.exr", "path");
+        TCLAP::UnlabeledValueArg<std::string> outputFileArg("Output", "Path to the OpenEXR image to write to.", true, "out.exr", "path");
+
+        cmd.add(inputFileArg);
+        cmd.add(outputFileArg);
+
+        std::vector<std::string> allowedCompressionConstraint;
+        allowedCompressionConstraint.push_back("none");
+        allowedCompressionConstraint.push_back("rle");
+        allowedCompressionConstraint.push_back("zips");
+        allowedCompressionConstraint.push_back("zip");
+        allowedCompressionConstraint.push_back("piz");
+        allowedCompressionConstraint.push_back("pxr24");
+        allowedCompressionConstraint.push_back("b44");
+        allowedCompressionConstraint.push_back("b44a");
+        allowedCompressionConstraint.push_back("dwaa");
+        allowedCompressionConstraint.push_back("dwab");
+
+        TCLAP::ValuesConstraint<std::string> allowedCompressionMethodsVals(allowedCompressionConstraint);
+        TCLAP::ValueArg<std::string> compressionMethodArg("m", "method", "Specify the compression method to use for the output OpenEXR image.", true, "zip", &allowedCompressionMethodsVals);
+
+        cmd.add(compressionMethodArg);
+
+        cmd.parse(argc, argv);
+
+        path_image_in = inputFileArg.getValue();
+        path_image_out = outputFileArg.getValue();
+
+        if (compressionMethodArg.getValue() == "none") {
+            compression = Imf::Compression::NO_COMPRESSION;
+        } else if (compressionMethodArg.getValue() == "rle") {
+            compression = Imf::Compression::RLE_COMPRESSION;
+        } else if (compressionMethodArg.getValue() == "zips") {
+            compression = Imf::Compression::ZIPS_COMPRESSION;
+        } else if (compressionMethodArg.getValue() == "zip") {
+            compression = Imf::Compression::ZIP_COMPRESSION;
+        } else if (compressionMethodArg.getValue() == "pxr24") {
+            compression = Imf::Compression::PXR24_COMPRESSION;
+        } else if (compressionMethodArg.getValue() == "b44") {
+            compression = Imf::Compression::B44_COMPRESSION;
+        } else if (compressionMethodArg.getValue() == "b44a") {
+            compression = Imf::Compression::B44A_COMPRESSION;
+        } else if (compressionMethodArg.getValue() == "dwaa") {
+            compression = Imf::Compression::DWAA_COMPRESSION;
+        } else if (compressionMethodArg.getValue() == "dwab") {
+            compression = Imf::Compression::DWAB_COMPRESSION;
+        } else {
+            std::cerr << "Error: unknown compression type: " << compressionMethodArg.getValue() << std::endl;
+            return 1;
+        }
+
+    }  catch (TCLAP::ArgException  &e) {
+        std::cerr << "Error: " << e.error() << " for argument " << e.argId() << std::endl;
+        return 1;
+    }
+
+
+    const EXRImage image_in(path_image_in);
+
+    EXRImage image_out(image_in.width(), image_in.height(), compression);
+
+    for (const EXRFramebuffer* fb: image_in.getFramebuffersConst()) {
+        image_out.appendFramebuffer(fb->getPixelDataConst(), fb->getName());
+    }
+
+    // TODO: backup original metadata
+
+    image_out.write(path_image_out);
+}
