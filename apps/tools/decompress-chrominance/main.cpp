634b19dd97295bb1e2b8da3cf4c74a01a8891c57
Alban Fichet <alban.fichet@gmx.fr>
2023-01-20 16:11:44 +0100

    Clean the chrominance extraction tool

--- apps/tools/decompress-chrominance/main.cpp
+++ apps/tools/decompress-chrominance/main.cpp
@@ -1,3 +1,36 @@
+/**
+ * Copyright 2022 - 2023 Alban Fichet
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ *   2. Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution.
+ *
+ *   3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <iostream>
 
 #include <JXLImage.h>
@@ -6,39 +39,27 @@
 #include <moments_image.h>
 #include <moments.h>
 
+#include <tclap/CmdLine.h>
+
 
 void decompress_spectral_framebuffer(
     const SGEGSpectralGroup& sg,
     const std::vector<std::vector<float>>& compressed_moments,
     std::vector<float>& spectral_framebuffer)
 {
-    const size_t n_pixels = compressed_moments[0].size();
-
-    size_t n_moments = 0;
-
-    switch (sg.method) {
-        case LINEAR:
-        case LINAVG:
-        case BOUNDED:
-        case UNBOUNDED:
-        case UNBOUNDED_TO_BOUNDED:
-            assert(sg.layer_indices.size() == (sg.mins.size() + 1));
-            assert(sg.layer_indices.size() == (sg.maxs.size() + 1));
-            n_moments = compressed_moments.size();
-            break;
-        case UPPERBOUND:
-        case TWOBOUNDS:
-            assert(sg.layer_indices.size() - 1 == (sg.mins.size() + 1));
-            assert(sg.layer_indices.size() - 1 == (sg.maxs.size() + 1));
-            n_moments = compressed_moments.size() - 1;
-            break;
-    }
+    const size_t n_pixels  = compressed_moments[0].size();
+    const size_t n_moments = compressed_moments.size();
 
     std::vector<double> wavelengths_d;
     std::vector<double> mins_d, maxs_d;
     std::vector<double> spectral_framebuffer_d;
     std::vector<double> compressed_moments_d(n_moments * n_pixels);
     std::vector<uint8_t> relative_scales;
+
+    if (sg.method != LINAVG) {
+        std::cerr << "Cannot perform decompression with this format." << std::endl;
+        return;
+    }
 
     Util::cast_vector(sg.wavelengths, wavelengths_d);
     Util::cast_vector(sg.mins, mins_d);
@@ -52,27 +73,13 @@ void decompress_spectral_framebuffer(
         compressed_moments_d[px * n_moments] = 0.5;
     }
 
-    if (sg.method == UPPERBOUND || sg.method == TWOBOUNDS) {
-        relative_scales.resize(n_pixels);
-
-        for (size_t px = 0; px < n_pixels; px++) {
-            relative_scales[px] = std::numeric_limits<uint8_t>::max() * compressed_moments[n_moments][px];
-        }
-    }
-
-    double timing;
-
-    decompress_spectral_image(
-        LINEAR,
+    linear_decompress_spectral_image(
         wavelengths_d,
         compressed_moments_d,
         mins_d, maxs_d,
-        relative_scales,
-        sg.global_min, sg.global_max,
         n_pixels,
         n_moments,
-        spectral_framebuffer_d,
-        timing
+        spectral_framebuffer_d
     );
 
     Util::cast_vector(spectral_framebuffer_d, spectral_framebuffer);
@@ -94,22 +101,32 @@ Imf::PixelType quantization_to_exr(size_t n_bits, size_t n_exponent_bits) {
 
 int main(int argc, char* argv[])
 {
-    if (argc < 3) {
-        std::cout << "Usage:" << std::endl
-                  << "------" << std::endl
-                  << argv[0] << " <jxl_in> <exr_out>" << std::endl;
-        exit(0);
+    std::string path_image_in, path_image_out;
+
+    // Parse arguments
+    try {
+        TCLAP::CmdLine cmd("Decompress an input image with a constant DC coefficient to get a chrominance like image.");
+
+        TCLAP::UnlabeledValueArg<std::string> inputFileArg ("Input", "Path to the input compressed spectral JPEG-XL image.", true, "input.jxl", "path");
+        TCLAP::UnlabeledValueArg<std::string> outputFileArg("Output", "Path to the output chrominance OpenEXR.", true, "output_chrominance.exr", "path");
+
+        cmd.add(inputFileArg);
+        cmd.add(outputFileArg);
+
+        cmd.parse(argc, argv);
+
+        path_image_in  = inputFileArg.getValue();
+        path_image_out = outputFileArg.getValue();
+    } catch (TCLAP::ArgException &e) {
+        std::cerr << "Error: " << e.error() << " for argument " << e.argId() << std::endl;
+        return 1;
     }
 
-    const char* filename_in  = argv[1];
-    const char* filename_out = argv[2];
-
-    const JXLImage jxl_image(filename_in);
+    const JXLImage jxl_image(path_image_in);
 
     const SGEGBox box   = jxl_image.getBox();
     const size_t width  = jxl_image.width();
     const size_t height = jxl_image.height();
-
 
     EXRSpectralImage exr_out(width, height);
 
@@ -160,8 +177,7 @@ int main(int argc, char* argv[])
         );
     }
 
-    exr_out.write(filename_out);
-
+    exr_out.write(path_image_out);
 
     return 0;
 }
