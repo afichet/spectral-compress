26fe78b2e82c8a4f1ef4624ca51557488b4a970a
Alban Fichet <alban.fichet@gmx.fr>
2023-01-23 15:01:09 +0100

    Adding the script to convert CAVE to spectral OpenEXR

---
+++ experiments/cave_to_exr.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+
+cave_png_dirs = '/home/afichet/spectral_images/CAVE'
+output_exr_dir = '/home/afichet/spectral_images/EXRs/CAVE'
+bin_dir = os.path.join('..', 'build', 'bin')
+
+os.makedirs(output_exr_dir, exist_ok=True)
+
+for f in os.listdir(cave_png_dirs):
+    print(f)
+    path_first_png = os.path.join(cave_png_dirs, f, f, f + '_01.png')
+    path_output    = os.path.join(output_exr_dir, f + '.exr')
+
+    subprocess.run([
+        os.path.join(bin_dir, 'cave-exr'),
+        path_first_png,
+        path_output,
+        '-t', 'float',
+        '-r'   # Do not generate RGB layers
+        ]
+    )
