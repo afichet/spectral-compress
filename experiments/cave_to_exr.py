7181ec71e045122d5e805756aa7a5381e52f5a59
Alban Fichet <alban.fichet@gmx.fr>
2023-01-27 01:58:32 +0100

    Let the RGB layer in this step to have constitency with the Bonn script

--- experiments/cave_to_exr.py
+++ experiments/cave_to_exr.py
@@ -7,18 +7,23 @@ cave_png_dirs = '/home/afichet/spectral_images/CAVE'
 output_exr_dir = '/home/afichet/spectral_images/EXRs/CAVE'
 bin_dir = os.path.join('..', 'build', 'bin')
 
-os.makedirs(output_exr_dir, exist_ok=True)
 
-for f in os.listdir(cave_png_dirs):
-    print(f)
-    path_first_png = os.path.join(cave_png_dirs, f, f, f + '_01.png')
-    path_output    = os.path.join(output_exr_dir, f + '.exr')
+def main():
+    os.makedirs(output_exr_dir, exist_ok=True)
 
-    subprocess.run([
-        os.path.join(bin_dir, 'cave-exr'),
-        path_first_png,
-        path_output,
-        '-t', 'float',
-        '-r'   # Do not generate RGB layers
-        ]
-    )
+    for f in os.listdir(cave_png_dirs):
+        print(f)
+        path_first_png = os.path.join(cave_png_dirs, f, f, f + '_01.png')
+        path_output    = os.path.join(output_exr_dir, f + '.exr')
+
+        subprocess.run([
+            os.path.join(bin_dir, 'cave-exr'),
+            path_first_png,
+            path_output,
+            '-t', 'float'
+            ]
+        )
+
+
+if __name__ == '__main__':
+    main()
